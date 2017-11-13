fed5edad353bedbb28c7df6c02e655daa7f92107
Unknown <unai.robles@ikasle.egibide.org>
2017-11-13 19:41:42 +0100

    04_CONVERTIR_TEMPERATURA

--- 04_convertir_temperatura/src/com/unairobles/Main.java
+++ 04_convertir_temperatura/src/com/unairobles/Main.java
@@ -1,19 +1,34 @@
 package com.unairobles;
 
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
+    public static void convertirTemperatura(double escala) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Escribe grados ºc a convertir: ");
+        double temperatura = Integer.parseInt(br.readLine());
+        if (escala == 1) {
+            System.out.println(1.8 * temperatura + 329);
+        } else {
+            System.out.println(temperatura + 273.15);
+        }
+    }
 
     public static void main(String[] args) throws IOException {
-	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	System.out.println("Temperatura en ºC: ");
-	double temperatura = Double.parseDouble(br.readLine());
-	System.out.println("Grados ºF: " +convertirTemperatura(temperatura));
+        System.out.println("=== Temperaturas ===");
+        System.out.println("1. Convertir a Fahrenheit");
+        System.out.println("2. Convertir a Kelvin");
+        System.out.println("3. Salir");
+        System.out.print("Opción:");
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        double escala = Integer.parseInt(br.readLine());
+        if (escala == 3) {
+        } else {
+            convertirTemperatura(escala);
+        }
+
     }
 
-    private static double convertirTemperatura(double temperatura) {
-        return temperatura*1.8+32;
-    }
 }
