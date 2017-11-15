573ef512373f3d52181266b0c63929258fed6924
Unknown <unai.robles@ikasle.egibide.org>
2017-11-15 18:58:52 +0100

    08_FACTORIAL(ST)

--- 08_factorial/src/com/unairobles/Main.java
+++ 08_factorial/src/com/unairobles/Main.java
@@ -5,12 +5,12 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-    public static int factorial(int numero1) throws IOException {
-        int resultado = 0;
+    public static int factorial(int numero1, int resultado) throws IOException {
         int i;
         for (i = 0; i <= numero1; i++) {
             resultado = numero1 * (numero1 - 1);
-            return numero1;
+            return resultado;
+            return  numero1;
         }
         return i;
     }
@@ -20,10 +20,10 @@ public class Main {
         System.out.println("Escribe numero del que calcular factorial: ");
         int numero1 = Integer.parseInt(br.readLine());
         int resultado = 0;
-        if (numero1 > 0) {
+        if (numero1 > 0){
             resultado = factorial(numero1);
-        } else {
-        }
+            numero1 = numero1 -1;
+        }else{};
         System.out.println("El resultado es " + resultado);
     }
 }
