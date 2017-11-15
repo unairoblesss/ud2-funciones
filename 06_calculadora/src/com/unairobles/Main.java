2c54b00d0d78e47d1590f4d6944a40576b70e289
Unknown <unai.robles@ikasle.egibide.org>
2017-11-15 19:30:40 +0100

    06_CALCULADORA{CORREGIDO CON ANOTACIONES!

--- 06_calculadora/src/com/unairobles/Main.java
+++ 06_calculadora/src/com/unairobles/Main.java
@@ -3,47 +3,60 @@ package com.unairobles;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Objects;
 
+//EN VEZ DE CHAR ESTA HECHO CON STRINGS
+//TIENE QUE DEVOLVERTE RESULTADO PARA MOSTRARLO EN Main Y DESPUES LOS MENSAJES EN LA MISMA, EN VEZ DE VOID TIENES QUE CREAR UN DOUBLE Y QUE TE DEVUELVA EL VALOR.
 public class Main {
     public static void longitud(int radio) throws IOException {
         System.out.println("La longitud de la circunferencia es " + radio * 2);
     }
 
     public static void area(int radio) throws IOException {
-        System.out.println("El area de la circunferencia es " + radio * radio * 3.1416);
+        System.out.println("El area de la circunferencia es " + radio * radio * Math.PI);//Para calcular numeros elevados Math.pow(r,2(3,4,5,n...)).
     }
 
     public static void volumen(int radio) throws IOException {
-        System.out.println("El volumen de la circunferencia es " + radio * radio * radio * 3.1416 * 4 / 3);
-    }
+        System.out.println("El volumen de la circunferencia es "//%f ,funcion() muestra el resultado de la funcion directamente
+                + radio * radio * radio * Math.PI * 4 / 3);
+    }//return = inventas variable y mostrar en funcion Main
 
     public static void main(String[] args) throws IOException {
-        System.out.println("=== Calculadora ===\n" +
-                "a) Longitud de la circunferencia\n" +
-                "b) Área del círculo\n" +
-                "c) Volumen de la esfera\n" +
-                "d) Salir\n" +
-                "Opción:");
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String opcion = br.readLine();
-        switch (opcion) {
-            case "a":
-                System.out.println("Escribe el radio: ");
-                int radio = Integer.parseInt(br.readLine());
-                longitud(radio);
-                break;
-            case "b":
-                System.out.println("Escribe el radio: ");
-                radio = Integer.parseInt(br.readLine());
-                area(radio);
-                break;
-            case "c":
-                System.out.println("Escribe el radio: ");
-                radio = Integer.parseInt(br.readLine());
-                volumen(radio);
-                break;
-            default:
-        }
+        String opcion;
+        do {
+            System.out.println("=== Calculadora ===\n" +
+                    "a) Longitud de la circunferencia\n" +
+                    "b) Área del círculo\n" +
+                    "c) Volumen de la esfera\n" +
+                    "d) Salir\n" +
+                    "Opción:");
+
+            opcion = br.readLine();//.toLowerCase().charArt(0);
+            switch (opcion) {
+                case "a"://si fueran char serian con comillas simples.
+                    System.out.println("Escribe el radio: ");
+                    int radio = Integer.parseInt(br.readLine());
+                    longitud(radio);
+                    break;
+                case "b":
+                    System.out.println("Escribe el radio: ");
+                    radio = Integer.parseInt(br.readLine());
+                    area(radio);
+                    break;
+                case "c":
+                    System.out.println("Escribe el radio: ");
+                    radio = Integer.parseInt(br.readLine());
+                    volumen(radio);
+                    break;
+                case "d":
+                    break;
+                default://Mensaje de error.
+            }
+        } while (!Objects.equals(opcion, "d")
+                );
     }
 }
+
+
 
