db67085f54ee4aa8e202bdcbbe8e5c1f39eb5ed7
Unknown <unai.robles@ikasle.egibide.org>
2017-11-15 21:36:05 +0100

    .

--- 08_factorial/src/com/unairobles/Main.java
+++ 08_factorial/src/com/unairobles/Main.java
@@ -5,25 +5,34 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-    public static int factorial(int numero1, int resultado) throws IOException {
-        int i;
-        for (i = 0; i <= numero1; i++) {
-            resultado = numero1 * (numero1 - 1);
-            return resultado;
-            return  numero1;
+    public static int iterativo(int numero1) throws IOException {
+        int resultado = 1;
+        for (int i = 1; i <= numero1; i++) {
+            resultado *= i;
         }
-        return i;
+        return resultado;
     }
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        System.out.println("Escribe numero del que calcular factorial: ");
-        int numero1 = Integer.parseInt(br.readLine());
-        int resultado = 0;
-        if (numero1 > 0){
-            resultado = factorial(numero1);
-            numero1 = numero1 -1;
-        }else{};
-        System.out.println("El resultado es " + resultado);
+    public static int factorial(int numero1) throws IOException {
+        if (numero1 < 0) {
+            return factorial(numero1 - 1) * numero1;
+        } else {
+            return 1;
+        }
+
     }
-}
+
+    public static void main(String[] args) throws IOException {//SIN CORREGIR
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));//SIN CORREGIR
+        System.out.println("Escribe numero del que calcular factorial: ");//SIN CORREGIR
+        int numero1 = Integer.parseInt(br.readLine());//SIN CORREGIR
+        int resultado = 0;//SIN CORREGIR
+        if (numero1 > 0) {//SIN CORREGIR
+            resultado = factorial(numero1);//SIN CORREGIR
+            numero1 = numero1 - 1;//SIN CORREGIR
+        } else {//SIN CORREGIR
+        }//SIN CORREGIR
+        ;//SIN CORREGIR
+        System.out.println("El resultado es " + resultado);//SIN CORREGIR
+    }//SIN CORREGIR
+}//SIN CORREGIR
