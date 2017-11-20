63797e3dfb3e3e5f5705ddd01d0a4c795d9baf6f
Unknown <unai.robles@ikasle.egibide.org>
2017-11-20 18:39:04 +0100

    08_FACTORIAL

---
+++ 09_fibonacci/src/com/unairobles/Main.java
@@ -0,0 +1,21 @@
+package com.unairobles;
+
+public class Main {
+
+    public static int fibonacci (int n){
+        if (n<2) {
+            return n;
+        } else {
+            return fibonacci(n-1)+fibonacci(n-2);
+        }
+    }
+
+    public static void main(String[] args) {
+
+        for (int i = 1; i < 10; i++) {
+            System.out.print(fibonacci(i)+" ");
+        }
+
+
+    }
+}
